12e64812a95cd1d3771ef7578ac7ac72e0f940e0
Mohammad Hamza Piracha <33203065+Hamju1999@users.noreply.github.com>
2025-04-05 01:33:19 -0500

    Update ChatbotWebApp.py

--- ChatbotWebApp.py
+++ ChatbotWebApp.py
@@ -39,7 +39,7 @@ def simplifytext(text, client, patientcontext=None):
     )
     try:
         response = client.chat.completions.create(
-            model="google/gemini-2.0-flash-thinking-exp:free",
+            model="deepseek/deepseek-r1:free",
             messages=[{"role": "user", "content": message}],
         )
         return response.choices[0].message.content
