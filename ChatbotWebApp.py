21f63296a3fc759455e5f473c511afaa1c5db2f5
Mohammad Hamza Piracha <33203065+Hamju1999@users.noreply.github.com>
2025-04-04 22:04:38 -0500

    Update ChatbotWebApp.py

--- ChatbotWebApp.py
+++ ChatbotWebApp.py
@@ -39,6 +39,11 @@ def simplifytext(text, client, patientcontext=None):
     )
     try:
         response = client.chat.completions.create(
+            extra_headers={
+                "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+                "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+              },
+            extra_body={},
             model="deepseek/deepseek-r1:free",
             messages=[{"role": "user", "content": message}],
         )
